0a87f7a0f39358aa619a7522214b6073b5095716
5he1d0r <5he11d000r@gmail.com>
2016-05-20 09:04:55 +0000

    Layout of GUI is completely new -Buttons of Top and Bottom align in center -FlowButtons with shadow -Bigger Fassung for the Possible FlowNodes -Grid has an bit 3D effect
    A file with the colors of the flows

--- src/de/adesso/flowsolver/gui/controler/FWController.java
+++ src/de/adesso/flowsolver/gui/controler/FWController.java
@@ -7,42 +7,46 @@ import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by slinde on 12.05.2016.
  */
-public class FWController {
-	
+public class FWController{
+
 	FlowWindow window;
 	private Stage primaryStage;
-	int heith = 600, length = 600;
-	
-	public void init(Stage primaryStage) {
+	int heith = 600 , length = 600;
+	List<Button> flowNodes = new LinkedList<>();
+
+	public void init(Stage primaryStage){
 		window = new FlowWindow(primaryStage, this, heith, length);
 		this.primaryStage = primaryStage;
 		window.init();
 		window.show();
 	}
-	
-	public void decrease() {
-		int i = Integer.parseInt(window.getGridSize()) - 1;
+
+	public void decrease(){
+		int i = Integer.parseInt(window.getGridSize())-1;
 		window.setGridSize(Integer.toString(i >= 5 ? i : 5));
 	}
-	
-	public void increase() {
-		int i = Integer.parseInt(window.getGridSize()) + 1;
+
+	public void increase(){
+		int i = Integer.parseInt(window.getGridSize())+1;
 		window.setGridSize(Integer.toString(i <= 15 ? i : 15));
 	}
-	
-	public void generate() {
+
+	public void generate(){
 		window.generateNotes();
 		window.generateTable();
 	}
-	
-	public int getAmountNotes() {
-		switch (Integer.parseInt(window.getGridSize())) {
+
+	public int getAmountNotes(){
+		switch(Integer.parseInt(window.getGridSize())){
 			case 5:
 				return 4;
 			case 6:
@@ -69,50 +73,71 @@ public class FWController {
 				return 0;
 		}
 	}
-	
-	public void reset() {
-		generate();
-	}
-	
-	public void solve() {
-	}
-	
-	public int getGridSize() {
-		return Integer.parseInt(window.getGridSize());
-	}
-	
-	public void resize() {
+
+	public void reset(){ generate(); }
+
+	public void solve(){}
+
+	public int getGridSize(){ return Integer.parseInt(window.getGridSize()); }
+
+	public void resize(){
 		int gridsize = getGridSize();
-		window.setWindowheigth(150 + (gridsize * 40));
-		window.setWindowwidth(250 + (gridsize * 40));
+		window.setWindowheigth(150 + (gridsize*40));
+		window.setWindowwidth(250 + (gridsize*40));
 		window.stageSize();
+		flowNodes.clear();
 	}
-	
-	public void draged(MouseEvent event, Button source) {
-	    /* drag was detected, start a drag-and-drop gesture*/
+
+	public void draged(MouseEvent event, Button source){
+        /* drag was detected, start a drag-and-drop gesture*/
         /* allow any transfer mode */
-		Dragboard db = source.startDragAndDrop(TransferMode.ANY);
+				Dragboard db = source.startDragAndDrop(TransferMode.ANY);
 
         /* Put a string on a dragboard */
-		ClipboardContent content = new ClipboardContent();
-		content.putString(source.getText());
-		db.setContent(content);
-		
-		event.consume();
+				ClipboardContent content = new ClipboardContent();
+				content.putString(source.getText());
+				db.setContent(content);
+
+				event.consume();
 	}
-	
-	public void droped(DragEvent event, GridPane target) {
-         /* data dropped */
+
+	public void droped(DragEvent event, Pane target){
+		 /* data dropped */
         /* if there is a string data on dragboard, read it and use it */
 		Dragboard db = event.getDragboard();
 		boolean success = false;
 		if (db.hasString()) {
+			Button b = getButton(db.getString());
+			flowNodes.add(b);
+			target.getChildren().add(b);
+
 			success = true;
 		}
         /* let the source know whether the string was successfully
          * transferred and used */
 		event.setDropCompleted(success);
-		
+
+		event.consume();
+	}
+
+	private Button getButton(String string){
+		List<Button> buttons = window.getPossibleNotes();
+		for(Button b : buttons){
+			if (b.getText() == string){
+				buttons.remove(b);
+				return b;
+			}
+		}
+		return null;
+	}
+
+	public void dropable(DragEvent event, Pane target){
+		if (event.getGestureSource() != target &&
+						event.getDragboard().hasString()) {
+            /* allow for both copying and moving, whatever user chooses */
+			event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+		}
+
 		event.consume();
 	}
 }
--- src/de/adesso/flowsolver/gui/functions/FlowWindow.java
+++ src/de/adesso/flowsolver/gui/functions/FlowWindow.java
@@ -1,8 +1,7 @@
 package de.adesso.flowsolver.gui.functions;
 
+
 import de.adesso.flowsolver.gui.controler.FWController;
-import java.util.LinkedList;
-import java.util.List;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,41 +11,46 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by slinde on 12.05.2016.
  */
-public class FlowWindow {
-	
+public class FlowWindow{
+
 	private Button btnUp, btnDown, btnGenerate, btnReset, btnSolve;
 	private TextField tfGridSize;
-	
-	private Stage        primaryStage;
+
+	private Stage primaryStage;
 	private FWController events;
-	private int          heigth, wight;
+	private int heigth, wight;
 	private HBox hbTop, hbBottom;
-	private VBox         vbLeft;
-	private FlowPane     fpNotes;
-	private GridPane     center;
+	private VBox vbLeft;
+	private FlowPane fpNotes;
+	private GridPane center;
 	private List<Button> possibleFlowNodes;
-	private VBox         vbUpDown;
-	
-	public FlowWindow(Stage stage, FWController eventhandler, int heigth, int wight) {
+	private VBox vbUpDown;
+	private HBox hbSize;
+
+	public FlowWindow(Stage stage, FWController eventhandler, int heigth, int wight){
 		primaryStage = stage;
 		events = eventhandler;
 		this.heigth = heigth;
 		this.wight = wight;
-		
+
 		primaryStage.setTitle("Flow Solver");
 	}
-	
-	public void init() {
+
+	public void init(){
 		BorderPane mainLayout = new BorderPane();
-		
+
 		topBox();
 		hbTop.setId("hbTop");
 		hbTop.getStyleClass().add("hb");
@@ -56,27 +60,24 @@ public class FlowWindow {
 		btnDown.getStyleClass().add("UpDown");
 		btnGenerate.setId("btnGenerate");
 		btnGenerate.setStyle("-fx-spacing: 10px;");
-		
+
 		leftBox();
 		vbLeft.setId("vbLeft");
-		
+
 		centerBox();
 		center.setId("center");
-		
+
 		bottomBox();
 		hbBottom.setId("hbBottom");
 		hbBottom.getStyleClass().add("hb");
-		
+
 		btnUp.setOnAction(e -> events.increase());
 		btnDown.setOnAction(e -> events.decrease());
 		btnGenerate.setOnAction(e -> events.generate());
-		
+
 		btnReset.setOnAction(e -> events.reset());
 		btnSolve.setOnAction(e -> events.solve());
-		
-		for (Button b : possibleFlowNodes) b.setOnDragDetected(e -> events.draged(e, b));
-		center.setOnDragDropped(e -> events.droped(e, center));
-		
+
 		mainLayout.setTop(hbTop);
 		mainLayout.setLeft(vbLeft);
 		mainLayout.setCenter(center);
@@ -86,14 +87,14 @@ public class FlowWindow {
 		scene.getStylesheets().add("de/adesso/flowsolver/gui/design/FWDesign.css");
 		primaryStage.setScene(scene);
 	}
-	
-	public void stageSize() {
+
+	public void stageSize(){
 		primaryStage.setHeight(heigth);
 		primaryStage.setWidth(wight);
 	}
-	
-	private void centerBox() {/*
-	    flow	1 2 3 4 5 ..  n
+
+	private void centerBox(){/*
+		flow	1 2 3 4 5 ..  n
 		1
 		2
 		3
@@ -104,108 +105,134 @@ public class FlowWindow {
 		 */
 		center = new GridPane();
 		center.setAlignment(Pos.CENTER);
-		
+
 		generateTable();
 	}
-	
-	public void generateTable() {
+
+	public void generateTable(){
 		int size = events.getGridSize();
-		
+
+		//TODO buttens aus dem grid entfernen bei neuem generieren
+		center.getChildren().clear();
 		center.getColumnConstraints().clear();
 		center.getRowConstraints().clear();
-		
-		for (int i = 0; i < size; i++) {
+
+		for(int i = 0; i < size; i++) {
 			center.getColumnConstraints().add(new ColumnConstraints(40));
 			center.getRowConstraints().add(new RowConstraints(40));
 		}
-		
+
+		for (int i = 0 ; i < size ; i++) {
+			for (int j = 0; j < size; j++) {
+				addPane(i, j);
+			}
+		}
+
 		events.resize();
 	}
-	
-	private void bottomBox() {
+
+	private void addPane(int colIndex, int rowIndex) {
+		GridPane pane = new GridPane();
+		pane.setId("pane");
+		pane.setOnDragDropped(e -> events.droped(e, pane));
+		pane.setOnDragOver(e -> events.dropable(e, pane));
+		center.add(pane, colIndex, rowIndex);
+	}
+
+	private void bottomBox(){
 		btnReset = new Button("Reset");
 		btnSolve = new Button("Solve");
-		
+
 		hbBottom = new HBox();
-		
+
 		hbBottom.getChildren().add(btnReset);
 		hbBottom.getChildren().add(btnSolve);
 	}
-	
-	private void leftBox() {
+
+	private void leftBox(){
 		possibleFlowNodes = new LinkedList<>();
-		
+
 		vbLeft = new VBox();
 		fpNotes = new FlowPane();
 		vbLeft.setPrefWidth(200);
-		
+
 		generateNotes();
-		
+
 		vbLeft.getChildren().add(fpNotes);
 	}
-	
-	public void generateNotes() {
+
+	public void generateNotes(){
 		possibleFlowNodes.clear();
 		fpNotes.getChildren().clear();
-		for (int i = 0; i < events.getAmountNotes(); i++) {
-			Button b1 = new Button("" + (char) ('A' + i));
+		for(int i = 0; i < events.getAmountNotes(); i++){
+			Button b1 = new Button("" + (char)('A' + i));
 			b1.setId("flowNode");
-			Button b2 = new Button("" + (char) ('A' + i));
+			Button b2 = new Button("" + (char)('A' + i));
 			b2.setId("flowNode");
-			
+
 			Button bb1 = new Button();
 			bb1.setId("fassung");
 			Button bb2 = new Button();
 			bb2.setId("fassung");
-			
+
 			possibleFlowNodes.add(b1);
 			possibleFlowNodes.add(b2);
-			
+
 			StackPane spstart = new StackPane();
 			spstart.getChildren().add(bb1);
 			spstart.getChildren().add(b1);
 			StackPane spend = new StackPane();
 			spend.getChildren().add(bb2);
 			spend.getChildren().add(b2);
-			spstart.setStyle("-fx-padding: 10px");
-			spend.setStyle("-fx-padding: 10px");
+			spstart.setId("possibleNode");
+			spend.setId("possibleNode");
 			fpNotes.getChildren().addAll(spstart, spend);
+
 		}
+			for(Button b : possibleFlowNodes) b.setOnDragDetected(e -> events.draged(e, b));
 	}
-	
-	private void topBox() {
+
+	private void topBox(){
 		tfGridSize = new TextField("5");
 		btnUp = new Button("v");
 		btnDown = new Button("v");
 		btnGenerate = new Button("Generate Grid");
-		
+
 		hbTop = new HBox();
+		hbSize = new HBox();
 		vbUpDown = new VBox();
 		
-		hbTop.getChildren().add(tfGridSize);
 		vbUpDown.getChildren().add(btnUp);
 		vbUpDown.getChildren().add(btnDown);
-		hbTop.getChildren().add(vbUpDown);
+
+		hbSize.getChildren().add(tfGridSize);
+		hbSize.getChildren().add(vbUpDown);
+		
+		hbTop.getChildren().add(hbSize);
 		hbTop.getChildren().add(btnGenerate);
 	}
-	
-	public void show() {
+
+	public void show(){
 		primaryStage.show();
 	}
-	
-	public String getGridSize() {
+
+	public String getGridSize(){
 		return tfGridSize.getText();
 	}
-	
-	public void setGridSize(String gridSize) {
+
+	public void setGridSize(String gridSize){
 		tfGridSize.setText(gridSize);
 	}
-	
-	public void setWindowheigth(int heigth) {
+
+	public void setWindowheigth(int heigth){
 		this.heigth = heigth;
 	}
-	
-	public void setWindowwidth(int width) {
+
+	public void setWindowwidth(int width){
 		this.wight = width;
+	}
+
+	public List<Button> getPossibleNotes(){
+		return possibleFlowNodes;
 	}
 }
